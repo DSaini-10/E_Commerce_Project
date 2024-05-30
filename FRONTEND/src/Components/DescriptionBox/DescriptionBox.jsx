8851783785ebac091603a51eff8f309fa42fcf1e
DSaini-10 <mightyhthor200@gmail.com>
2024-05-31 00:35:58 +0530

    Initial Frontend Code

---
+++ FRONTEND/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './DescriptionBox.css'
+const DescriptionBox = () => {
+  return (
+    <div className="descriptionbox">
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem minus, impedit distinctio, nesciunt similique esse quas exercitationem aperiam iure facilis dignissimos aut aliquam adipisci eaque eius repellat expedita, numquam eum. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates eveniet a quo totam itaque, quibusdam omnis iure architecto temporibus doloribus corporis optio sapiente ducimus aut deleniti accusamus dignissimos? Nam, quo?</p>
+            
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat unde facere et molestiae harum, fugiat sint corrupti eum labore? Cupiditate beatae dolorem blanditiis dolor quae ratione! Autem dignissimos vel rem?</p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
